ea77c0a7cf9079464dc3c680b28babac7cd73be5
Your Name <you@example.com>
2024-07-08 16:32:35 +0530

    add: form-handling

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../common/components/button";
 
 function Signup() {
+  const [userInfo, setUserInfo] = useState({});
+
+  const handleChange = (event) => {
+    console.log("event******", event);
+    const inputValues = {
+      fName: event.target.value,
+    };
+    setUserInfo(inputValues);
+  };
+
+  const handleSubmit = () => {
+    console.log("userInfo******", userInfo);
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+  };
+
   return (
     <div>
       <label>First Name:</label>
-      <input name="fName"></input>
+      <input name="fName" onChange={(event) => handleChange(event)}></input>
+      <Button onClick={handleSubmit} />
     </div>
   );
 }
