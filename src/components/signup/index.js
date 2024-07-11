0b7c3974fa00674827e62cea7a761ee95709eb0f
Your Name <you@example.com>
2024-07-11 16:28:50 +0530

    feat: error handling

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -2,26 +2,60 @@ import React, { useState } from "react";
 import Button from "../../common/components/button";
 
 function Signup() {
-  const [userInfo, setUserInfo] = useState({});
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState({});
+  console.log("err*******", error);
 
-  const handleChange = (event) => {
-    console.log("event******", event);
-    const inputValues = {
-      fName: event.target.value,
-    };
-    setUserInfo(inputValues);
-  };
-
-  const handleSubmit = () => {
-    console.log("userInfo******", userInfo);
-    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+  const submit = () => {
+    if (firstName === "") {
+      setError({ fName: "* Please enter first name" });
+    }
+    if (lastName === "") {
+      setError({ lName: "* Please enter last name" });
+    } else {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          fName: firstName,
+          lName: lastName,
+          email: email,
+        })
+      );
+    }
   };
 
   return (
-    <div>
-      <label>First Name:</label>
-      <input name="fName" onChange={(event) => handleChange(event)}></input>
-      <Button onClick={handleSubmit} />
+    <div style={{ display: "flex", flexDirection: "column", width: "200px" }}>
+      <label>
+        First Name:{" "}
+        <span style={{ color: "red", fontSize: "10px" }}>{error.fName}</span>
+      </label>
+      <input
+        id="fName"
+        name="fName"
+        onChange={(e) => {
+          setFirstName(e.target.value);
+          setError("");
+        }}
+      ></input>
+      <label>
+        Last Name:
+        <span style={{ color: "red", fontSize: "10px" }}>{error.lName}</span>
+      </label>
+      <input
+        id="lName"
+        name="lName"
+        onChange={(e) => setLastName(e.target.value)}
+      ></input>
+      <label>Email:</label>
+      <input
+        id="email"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <Button onClick={submit}></Button>
     </div>
   );
 }
