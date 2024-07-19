ea7e2330630c953333635376b57bbb925090257d
Your Name <bhaumiksavliya1@gmail.com>
2024-07-19 11:35:14 +0530

    add: redux API call

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUsers } from "../../lib/redux/reducers/common";
 
 function Users() {
   const userList = JSON.parse(localStorage.getItem("user"));
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getUsers())
+  }, [])
+  
   return (
     <table>
       {userList?.length > 0 ? (
--- src/lib/redux/reducers/common.js
+++ src/lib/redux/reducers/common.js
@@ -1,9 +1,19 @@
-import { createSlice, current } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, current } from "@reduxjs/toolkit";
+
+// First, create the thunk
+export const getUsers = createAsyncThunk("common/getUsers", async () => {
+  console.log('api thunk call***');
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data;
+});
 
 const CommonSlice = createSlice({
   name: "common",
   initialState: {
+    loading: 'init',
     counter: 0,
+    users: [],
   },
   reducers: {
     increment: (state, action) => {
@@ -13,7 +23,25 @@ const CommonSlice = createSlice({
       state.counter = state.counter - action.payload;
     },
   },
-  //   extraReducers: {},
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(getUsers.pending, (state, action) => {
+      // Add user to the state array
+      console.log('api call pending***');
+      state.loading = 'pending';
+    });
+    builder.addCase(getUsers.fulfilled, (state, action) => {
+      // Add user to the state array
+      console.log('api call fulfilled***');
+      state.loading = 'fulfilled'
+      state.users = action.payload
+    });
+    builder.addCase(getUsers.rejected, (state, action) => {
+      // Add user to the state array
+      console.log('api call rejected***');
+      state.loading = 'rejected'
+    });
+  },
 });
 
 export const { increment, decrement } = CommonSlice.actions;
