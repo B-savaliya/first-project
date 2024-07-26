217a718f9f654d6726f2a09dc23d64038ab5f4de
Your Name <bhaumiksavliya1@gmail.com>
2024-07-26 15:59:05 +0530

    add: use ref completed

--- src/components/RefComp/index.js
+++ src/components/RefComp/index.js
@@ -1,15 +1,40 @@
-import React, { useEffect, useRef } from "react";
+import React, { forwardRef, useEffect, useRef } from "react";
 
 function UseRefComp() {
-  const myRef = useRef(null);
+  const myRef = useRef({});
+  const inputRef = useRef({});
+
   useEffect(() => {
-    console.log("******", (myRef.current.style.color = "red"));
+    myRef.current.style.color = "red";
   }, []);
+
+  const handleChange = (event) => {
+    inputRef.current = {
+      [event.target.name]: event.target.value,
+    };
+  };
+
+  const handleClick = () => {
+    console.log("ref_value********", inputRef.current);
+  };
+
   return (
     <div>
-      <p ref={myRef}>This is paragraph</p>
+      <p>This is paragraph</p>
+      <input type="text" name="input" onChange={handleChange} />
+      <button onClick={handleClick}>Click me</button>
+      name: {inputRef.current.input}
+      <Child ref={myRef} />
     </div>
   );
 }
 
 export default UseRefComp;
+
+const Child = forwardRef((props, ref) => {
+  return (
+    <div>
+      <p ref={ref}>Child</p>
+    </div>
+  );
+});
